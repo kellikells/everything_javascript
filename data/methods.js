a8a3a54fdea3d2c781996cc3460ba7e6e0cc2533
kelli <kellikells@gmail.com>
2021-06-08 17:14:47 -0700

    added method data

---
+++ data/methods.js
@@ -0,0 +1,48 @@
+export const methodData = [
+
+    {
+        title: 'Array - push',
+        functionName: 'addWithPush(arr, item)',
+        question: 'adds <item> to the end of array <arr>',
+        answer: `
+            return arr.push(item);
+        }
+        `,
+    },
+    {
+        title: 'Array - pop',
+        functionName: 'removeWithPop(arr)',
+        question: 'removes the last item from the end of an array <arr>',
+        answer: `
+            return arr.pop();
+        }
+        `,
+    },
+    {
+        title: 'Array - shift',
+        functionName: 'removeWithShift(arr)',
+        question: 'removes the first item in the array <arr>',
+        answer: `
+            return arr.shift();
+        }
+        `,
+    },
+    {
+        title: 'Array - unshift',
+        functionName: 'addWithShift(arr,item)',
+        question: 'adds <item> to the beginning of <arr>',
+        answer: `
+            return arr.unshift(item);
+        }
+        `,
+    },
+    // {
+    //     title: 'Array - ',
+    //     functionName: '',
+    //     question: '',
+    //     answer: `
+            
+    //     }
+    //     `,
+    // },
+]
