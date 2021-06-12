c3db3af381b181aaa674df2baea28ef3480d031e
kelli <kellikells@gmail.com>
2021-06-12 12:01:16 -0700

    changed text for next and previous button components

--- components/buttons/NextButton.js
+++ components/buttons/NextButton.js
@@ -3,13 +3,13 @@ export default function NextButton({ handleClick, disabled }) {
         <button
             onClick={handleClick}
             disabled={disabled}
+            value='md:Next' 
             className=
             {disabled
                 ? 'w-full py-2 md:py-4 bg-gray-300 opacity-25'
                 : 'w-full py-2 md:py-4 bg-green-500 text-white text-lg opacity-75 hover:opacity-100'
             }
-        >
-            Next 
+        >       
             <i className="fas fa-arrow-right ml-1"></i>
         </button>
     )
--- components/buttons/PreviousButton.js
+++ components/buttons/PreviousButton.js
@@ -3,15 +3,15 @@ export default function PreviousButton({ handleClick, disabled }) {
         <button
             onClick={handleClick}
             disabled={disabled}
+            value='md:Previous'
             className=
             {disabled
                 ? 'w-full py-2 md:py-4 bg-gray-300 opacity-25'
                 : 'w-full py-2 md:py-4 bg-green-500 text-white text-lg opacity-75 hover:opacity-100'
-
             }
         >
             <i class="fas fa-arrow-left mr-1"></i>
-            Previous
+    
         </button>
     )
 }
