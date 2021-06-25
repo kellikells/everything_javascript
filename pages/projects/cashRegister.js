517815b40bc2c5b1acc1d20fa02a242810e34d54
kelli <kellikells@gmail.com>
2021-06-24 22:46:49 -0700

    finished roman numerals and cash register

---
+++ pages/projects/cashRegister.js
@@ -0,0 +1,131 @@
+// Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+// cid is a 2D array listing available currency.
+
+// The checkCashRegister() function should always return an object with a status key and a change key.
+
+// Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+// Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+// Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+
+// See below for an example of a cash -in -drawer array:
+// [
+//     ["PENNY", 1.01],
+//     ["NICKEL", 2.05],
+//     ["DIME", 3.1],
+//     ["QUARTER", 4.25],
+//     ["ONE", 90],
+//     ["FIVE", 55],
+//     ["TEN", 20],
+//     ["TWENTY", 60],
+//     ["ONE HUNDRED", 100]
+// ]
+// ________________________________
+
+
+function checkCashRegister(price, cash, cid) {
+    const UNIT_VALUES = [0.01, 0.05, 0.10, 0.25, 1.00, 5.00, 10.00, 20.00, 100.00];
+    console.log(cid);
+    // let updatedCid = JSON.parse(JSON.stringify(cid));
+    let mutatedCid = JSON.parse(JSON.stringify(cid));
+    var change = {
+        status: 'INSUFFICIENT_FUNDS',
+        change: [],
+    };
+    let cidTotal = 0;
+
+    // 1- get the cid total
+    for (let i = 0; i < cid.length; i++) {
+        // console.log(`line41: ${(cid[i][1])}`)
+        cidTotal += (cid[i])[1];
+    }
+    cidTotal = cidTotal.toFixed(2);
+    // console.log(`line45: cidTotal: ${cidTotal}`);
+
+    let unitCountArr = mutatedCid.map((unit, index) => {
+        return Math.round(unit[1] /= UNIT_VALUES[index]);
+    })
+    // console.log(`line40: unitCountArr: ${unitCountArr}`)
+
+    // 2- get (cash-price)
+    let changeNeeded = cash - price;
+    // console.log(`line52: changeNeeded: ${changeNeeded}`);
+
+
+    // ********* if not enough cid 
+    if (changeNeeded > cidTotal) {
+        change.status = 'INSUFFICIENT_FUNDS';
+        change.change = [];
+
+        return change;
+    }
+
+    // ********* if (cash-price) == cid     // {status: "CLOSED", change: [<cid>]}
+    if (changeNeeded == cidTotal) {
+        change.status = 'CLOSED';
+        change.change = cid;
+        // change.change = updatedCid;
+        console.log(change);
+        return change;
+    }
+
+    // try to make the cashNeeded from cidTotal units
+    // start from large units, and add as appropriate
+    let currTotalFromCid = 0.00;
+    let currChangeArr = [];
+    for (let i = cid.length - 1; i >= 0; i--) {
+
+        let currCount = 0;
+        let currAmount = 0;
+        while (((currTotalFromCid + UNIT_VALUES[i]).toFixed(2) <= changeNeeded) && (unitCountArr[i] > 0)) {
+        // while ((currTotalFromCid + UNIT_VALUES[i] <= changeNeeded) && (unitCountArr[i] > 0)) {
+            currTotalFromCid += UNIT_VALUES[i];
+            // currTotalFromCid = currTotalFromCid.toFixed(2);
+            unitCountArr[i]--;
+            currCount++;
+            currAmount += UNIT_VALUES[i];
+            console.log(`currTotalFromCid = ${currTotalFromCid}`);
+            console.log(`unitCountArr: ${unitCountArr}`);
+        }
+        if (currCount > 0) {
+            currChangeArr.push([(cid[i])[0], currAmount]);
+        }
+
+        // ********* if changeNeeded can be made from cid  // {status: "OPEN", change: [<price-cash> ordered highest to lowers]},
+        console.log(currTotalFromCid.toFixed(2))
+        if (currTotalFromCid.toFixed(2) == changeNeeded) {
+            change.status = 'OPEN';
+            change.change = currChangeArr;
+            console.log(change)
+            return change;
+        }
+    }
+    console.log(`the end ${change}`)
+    return change;
+
+}
+
+
+
+// ________________________________
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+    //should return { status: "OPEN", change: [["QUARTER", 0.5]] }.
+    
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+    //{status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+// console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+//     //{status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+//     //{status: "INSUFFICIENT_FUNDS", change: []}.
+
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+//     //{status: "INSUFFICIENT_FUNDS", change: []}.
+
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]))
+//     //{status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
--- pages/projects/romanNumerals.js
+++ pages/projects/romanNumerals.js
@@ -11,16 +11,8 @@
 
 // 1-3, 4-8, 9-13
 
-// if the length of 'whatever' is greater than 3, switch to ... 
-
-// const romanNums = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-// }
 
 let I = [1, 'I'];
-
 let V = [5, 'V'];
 let X = [10, 'X'];
 let L = [50, 'L'];
@@ -28,58 +20,58 @@ let C = [100, 'C'];
 let D = [500, 'D'];
 let M = [1000, 'M'];
 
-romanArr = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-numsArr = [1000, 500, 100, 50, 10, 5, 1];
+let romanArr = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
+let numsArr = [1000, 500, 100, 50, 10, 5, 1];
 
-
-
-
-// -----------------------------
-let letter = romanArr[0]
-
-// const RN_OBJ = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50,
-//     C: 100,
-//     D: 500,
+// const ROMAN_NUMS_OBJ = {
 //     M: 1000,
+//     D: 500,
+//     C: 100,
+//     L: 50,
+//     X: 10,
+//     V: 5,
+//     I: 1,
 // }
-const RN_OBJ = {
+const ROMAN_NUMS_OBJ = {
     M: 1000,
+    CM: 900,
     D: 500,
+    CD: 400,
     C: 100,
+    XC: 90,
     L: 50,
+    XL: 40,
     X: 10,
+    IX: 9,
     V: 5,
+    IV: 4,
     I: 1,
 }
+
+// I, II, III, IV, V, VI, VII, VIII, IX, X 
 
 function convertToRoman(num) {
     let currentTotal = 0;
     let str = '';
-    for (let key in RN_OBJ) {
+    for (let key in ROMAN_NUMS_OBJ) {
 
+        // if 4, 9, 40, 90,400,900
+        // these are exceptions
+        while (num - ROMAN_NUMS_OBJ[key] >= 0) {
 
-        // if(RN_OBJ[key] <= num) {
-        while ((currentTotal + RN_OBJ[key]) <= num) {
-            currentTotal += RN_OBJ[key];
-
-            // check if the new currentTotal needs to be formatted
-            // check: units, tens, hundreds, thousands
-            // 4 or 9 ? then format, and break to the (for...in)
-
-            let stringTotal = String(currentTotal);
-            
-
-
+            num -= ROMAN_NUMS_OBJ[key];
             str += key;
-            // console.log(str)
-            // console.log(`${key} : ${RN_OBJ[key]}`);
 
+            // if (num == 0) return str;
+            // console.log(num);
+            if (num == 0) break;
         }
-        // return str
+
+        // while ((currentTotal + ROMAN_NUMS_OBJ[key]) <= num) {
+        //     currentTotal += ROMAN_NUMS_OBJ[key];
+        //     str += key;
+        //     console.log(`${key}: ${str}`);
+        // }
     }
     return str;
 }
@@ -91,8 +83,8 @@ function convertToRoman(num) {
 
 
 // console.log(convertToRoman(2))  // II.
-console.log(convertToRoman(3))  // III.
-console.log(convertToRoman(4))  // IV.
+// console.log(convertToRoman(3))  // III.
+// console.log(convertToRoman(4))  // IV.
 // console.log(convertToRoman(5))  // V.
 // console.log(convertToRoman(9))  // IX.
 // console.log(convertToRoman(12)) // XII.
@@ -102,7 +94,7 @@ console.log(convertToRoman(4))  // IV.
 // console.log(convertToRoman(45)) // XLV.
 // console.log(convertToRoman(68)) // LXVIII
 // console.log(convertToRoman(83)) // LXXXIII
-// console.log(convertToRoman(97)) // XCVII
+console.log(convertToRoman(97)) // XCVII
 // console.log(convertToRoman(99)) // XCIX
 // console.log(convertToRoman(400))    // CD
 // console.log(convertToRoman(500))    // D
